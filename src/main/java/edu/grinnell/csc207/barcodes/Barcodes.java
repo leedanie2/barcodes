ab6d0c766823e5733d1f81a8cdff2e5ab1438770
Daniel <leedanie2@grinnell.edu>
2026-02-01 22:55:58 -0600

    Finished Barcodes functions, no documentation yet

--- src/main/java/edu/grinnell/csc207/barcodes/Barcodes.java
+++ src/main/java/edu/grinnell/csc207/barcodes/Barcodes.java
@@ -1,9 +1,12 @@
 package edu.grinnell.csc207.barcodes;
 
 public class Barcodes {
-    /** TODO: fill in this definition and doc comment! */
+    /** Encodings for UPC-A digits 0 - 9 */
     private static int[][] ENCODINGS = {
-        // TODO: fill me in!
+        {3, 2, 1, 1}, {2, 2, 2, 1}, {2, 1, 2, 2}, 
+        {1, 4, 1, 1}, {1, 1, 3, 2}, {1, 2, 3, 1}, 
+        {1, 1, 1, 4}, {1, 3, 1, 2}, {1, 2, 1, 3}, 
+        {3, 1, 1, 2}
     };
 
     /**
@@ -21,7 +24,7 @@ public class Barcodes {
             return false;
         }
         for(int i = 0; i < 12; i++) {
-            if(!Character.isDigit(toDigit(code.charAt(i)))) {
+            if(!Character.isDigit(code.charAt(i))) {
                 return false;
             }
         }
@@ -33,9 +36,12 @@ public class Barcodes {
      */
     public static int computeCheckDigit(String code) {
         int C = 0;
-        for(int i = 0; i < 10; i++) {
-            C += 3 * toDigit(code.charAt(i));
-            C += toDigit(code.charAt(++i));
+        for(int i = 0; i < 11; i++) {
+            if (i % 2 == 0) {
+                C += 3 * toDigit(code.charAt(i));
+            } else {
+                C += toDigit(code.charAt(i));
+            }
         }
         C = C % 10;
         if (C == 0) {
@@ -46,11 +52,73 @@ public class Barcodes {
     }
 
     public static void printBlack() {
-        System.out.println("\033[30m█\033[0m");
+        System.out.print("\033[30m█\033[0m");
     }
 
     public static void printWhite() {
-        System.out.println("\033[37m█\033[0m");
+        System.out.print("\033[37m█\033[0m");
+    }
+
+
+    public static void printQuietZone() {
+        for(int i = 0; i < 9; i++) {
+            printWhite();
+        }
+    }
+
+    public static void printStartZone() {
+        printBlack();
+        printWhite();
+        printBlack();
+    }
+
+    public static void printDigit(int digit, boolean inverted) {
+        int[] digits = ENCODINGS[digit];
+        for(int i = 0; i < 4; i++) {
+            for(int j = 0; j < digits[i]; j++) {
+                if(!inverted) {
+                    printWhite();
+                } else {
+                    printBlack();
+                }
+            }
+            inverted = !inverted; // flip between white and black every iteration
+        }
+    }
+
+    public static void printLeftSide(String digits) {
+        for(int i = 0; i < 6; i++) {
+            int digit = toDigit(digits.charAt(i));
+            printDigit(digit, false); // not inverted
+        }
+    }
+
+    public static void printMiddleZone() {
+        for(int i = 0; i < 5; i++) {
+            if(i % 2 == 0) {
+                printWhite();
+            } else {
+                printBlack();
+            }
+        }
+    }
+
+    public static void printRightSide(String digits) {
+        for(int i = 0; i < 6; i++) {
+            int digit = toDigit(digits.charAt(i));
+            printDigit(digit, true); // inverted
+        }
+    }
+
+    public static void printBarcodeRow(String code) {
+        printQuietZone();
+        printStartZone();
+        printLeftSide(code.substring(0,6));
+        printMiddleZone();
+        printRightSide(code.substring(6));
+        printStartZone(); // endzone is same as startzone
+        printQuietZone();
+        System.out.println(); // newline
     }
 
     /**
@@ -58,6 +126,32 @@ public class Barcodes {
      * @param args
      */
     public static void main(String[] args) {
-        
+        if(args.length != 2) {
+            System.out.println("Usage: barcode <UPC-A code> <height>");
+            System.exit(1);
+        }
+
+        String code = args[0];
+        if(!isValidCode(code)) {
+            System.out.println("Code must be a string of 12 digits.");
+            System.exit(1);
+        }
+
+        int height = Integer.parseInt(args[1]);
+        if(height <= 0) {
+            System.out.println("Height must be a positive integer.");
+            System.exit(1);
+        }
+
+        int expected = computeCheckDigit(code);
+        int actual = toDigit(code.charAt(11)); // last digit is check digit
+        if(expected != actual) {
+            System.out.println("Expected check digit " + expected + " but found " + actual + ".");
+            System.exit(1);
+        }
+
+        for(int i = 0; i < height; i++) {
+            printBarcodeRow(code);
+        }
     }
 }
